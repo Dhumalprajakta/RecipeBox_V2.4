1f4363203e1738905b9c46a2bf364226a2bad779
Prajakta Dhumal <124650559+Dhumalprajakta@users.noreply.github.com>
2023-07-04 23:39:10 +0530

    first commit

---
+++ app/src/main/java/com/example/util/FavClickListener.java
@@ -0,0 +1,5 @@
+package com.example.util;
+
+public interface FavClickListener {
+    void onItemClick(boolean isFav, String message);
+}
---
+++ app/src/main/java/com/example/util/OnClick.java
@@ -0,0 +1,5 @@
+package com.example.util;
+
+public interface OnClick {
+    void position(int position, String type, String id, String title);
+}
